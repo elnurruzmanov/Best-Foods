25961d28f9cd1bed91a06daa6e728702fbf6031b
Elnur Ruzmanov <80697079+elnurruzmanov@users.noreply.github.com>
2022-11-27 10:39:39 +0300

    I created a folder Hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      <div>
+        {/* Overlay */}
+        <div>
+          <h1>The <span>Best</span></h1>
+          <h1>Foods <span>Delivery</span></h1>
+        </div>
+        <img src="" alt="/" />
+      </div>
+    </div>
+  )
+}
+
+export default Hero
